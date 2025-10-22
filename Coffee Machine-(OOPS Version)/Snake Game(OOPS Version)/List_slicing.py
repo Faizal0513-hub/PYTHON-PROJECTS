65f0fca1b947180b63748b05bec1d0f109fc366c
Faizal0513-hub <faizalautomation0513@gmail.com>
2025-10-22 10:41:19 +0530

    Delete Coffee Machine-(OOPS Version)/Snake Game(OOPS Version)/List_slicing.py

--- Coffee Machine-(OOPS Version)/Snake Game(OOPS Version)/List_slicing.py
+++
@@ -1,5 +0,0 @@
-alpha = ["a","b","c","d","f","e","f","g"]
-# print(alpha[::-1]) reversing a list
-# print(alpha[2:5]) number 5 will not be counted
-print(alpha[2:5:2])
-#Todo: tuples can also be sliced
